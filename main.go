4d3560b5e54a5362dec7ec20afd362947b142bd6
Ishan Sourav <ishansourav7@gmail.com>
2020-10-18 12:06:54 +0530

    homePage req handler created in main.go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 func main() {
 	fmt.Println("hello, world in Go")
+	handleRequests()
 }
